c055a999e6924a15c4016a121c2ed5a13ccb0792
Patrick Hettich <patrick_hettich@hotmail.com>
2021-02-02 13:35:30 +0100

    Added provider for shared_preferences, moved documentation

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spaceshooter/providers/preferences_provider.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ChangeNotifierProvider(
+      create: (ctx) => PreferenceProvider(),
+      child: Container(),
+    );
   }
 }
