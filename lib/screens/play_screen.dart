3d429d8bc04feb06c2a12acdcd3d2f0ad16f6f28
Patrick Hettich <patrick_hettich@hotmail.com>
2021-02-02 15:15:14 +0100

    added play-screen layout

--- lib/screens/play_screen.dart
+++ lib/screens/play_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:spaceshooter/widgets/game_field.dart';
 
 class PlayScreen extends StatelessWidget {
   static const ROUTE_NAME = '/play';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Punkte: '),
+        actions: [
+          Icon(Icons.favorite),
+          Icon(Icons.favorite),
+          Icon(Icons.favorite)
+        ],
+      ),
+      body: GameField(),
+    );
   }
 }
---
+++ lib/widgets/game_field.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GameField extends StatefulWidget {
+  @override
+  _GameFieldState createState() => _GameFieldState();
+}
+
+class _GameFieldState extends State<GameField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
