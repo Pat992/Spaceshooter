7db44b632aff21519247828318c98877f1f5934d
Patrick Hettich <patrick_hettich@hotmail.com>
2021-02-04 14:55:04 +0100

    updated start-screen, added particle effect for background

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_particle_background/flutter_particle_background.dart';
 import 'package:provider/provider.dart';
 import 'package:spaceshooter/providers/images_provider.dart';
 import 'package:spaceshooter/screens/play_screen.dart';
@@ -8,34 +9,48 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
-          Text(
-            'Spaceshooter',
+          ParticleBackground(
+            backgroundColor: Colors.black,
+            multiColor: true,
+            numberOfParticles: 50,
+            biggestSize: 10,
+            smallestSize: 1,
+            blur: true,
+            allFilled: true,
+            highestSpeed: 0.5,
+            slowestSpeed: 0.2,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton.icon(
-                icon: Icon(Icons.list),
-                label: Text('Highscores'),
-                onPressed: () =>
-                    Navigator.pushNamed(context, ScoreScreen.ROUTE_NAME),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              ElevatedButton.icon(
-                icon: Icon(Icons.play_arrow),
-                label: Text('New Game'),
-                onPressed: () =>
-                    Navigator.pushNamed(context, PlayScreen.ROUTE_NAME),
-              ),
-            ],
-          )
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    'Spaceshooter',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.play_arrow),
+                  label: Text('New Game'),
+                  onPressed: () =>
+                      Navigator.pushNamed(context, PlayScreen.ROUTE_NAME),
+                ),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.list),
+                  label: Text('Highscores'),
+                  onPressed: () =>
+                      Navigator.pushNamed(context, ScoreScreen.ROUTE_NAME),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
